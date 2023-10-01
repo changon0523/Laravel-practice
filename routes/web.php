c6614fdf2eb88928ad46e0682f0ec40ec45cbbdb
reiyachinen <rei.c0523@gmail.com>
2023-10-01 16:26:11 +0900

    for making controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function() {
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/', function () {
     return view('welcome');
 });
